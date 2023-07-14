8bed08e1edc7fbeac172ec8d1ae1accada0e7fcb
Gaurav <gaurav.chandaria@aurionpro.com>
2023-07-14 15:17:11 +0530

    DemoApp

--- Objective C/Auropay ObjC Demo/Auropay ObjC Demo/ViewController.m
+++ Objective C/Auropay ObjC Demo/Auropay ObjC Demo/ViewController.m
@@ -50,10 +50,10 @@
 - (IBAction)onTapPay:(UIButton *)sender {
     
     //Create Theme
-    APTheme *theme = [[APTheme alloc] initWithPrimaryColor:[UIColor redColor]
-                     primaryTextColor:[UIColor whiteColor]
-                     secondaryColor:[UIColor blueColor]
-                     secondaryTextColor:[UIColor whiteColor]];
+    APTheme *theme = [[APTheme alloc] initWithPrimaryColor:[self colorFromHexString:@"#236C6C"]
+                                          primaryTextColor:[UIColor whiteColor]
+                                          secondaryColor:[UIColor redColor]
+                                          secondaryTextColor:[UIColor whiteColor]];
     
     //Initilize the AuropayBuilder with subdomain Id, accessKey and secretKey
     Auropay * apb = [[[[[[[[[AuroPayBuilder alloc]
@@ -91,5 +91,12 @@
     NSLog(@"%@",message);
 }
 
-
+///UIColor hex to color
+-(UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
 @end
